746fc9bf28d267e05a257a27d8b90e6b32dac69c
Pritam Kalbhor <physics.pritam@gmail.com>
2022-05-23 16:41:11 +0530

    Create relval_alca.py : List of workflow IDs

---
+++ relval_alca.py
@@ -0,0 +1,30 @@
+from alcaval_steps import *
+workflows = Matrix()
+
+# CRUZET 2021
+workflows[1.1] = ['CRUZET2022', ['Cosmics2021', 'HLT_CRUZET2021', 'RECO_CRUZET2021', 'HARVEST_CRUZET2021']]
+workflows[1.2] = ['CRUZET2022', ['Cosmics2021', 'RECO_CRUZET2021', 'HARVEST_CRUZET2021']]
+workflows[1.3] = ['CRUZET2022', ['ExpressCosmics2021', 'HLT_CRUZET2021', 'RECO_CRUZET2021', 'HARVEST_CRUZET2021']]
+workflows[1.4] = ['CRUZET2022', ['ExpressCosmics2021', 'RECO_CRUZET2021', 'HARVEST_CRUZET2021']]
+
+# PBT 2021
+workflows[2.1] = ['PBT2021', ['MinimumBias2021', 'HLT_PBT2021', 'RECO_PBT2021', 'HARVEST_PBT2021']]
+workflows[2.2] = ['PBT2021', ['MinimumBias2021', 'RECOPE_PBT21', 'HARVEST_PBT2021']]
+workflows[2.3] = ['PBT2021', ['ZeroBias2021', 'HLT_PBT2021', 'RECO_PBT2021', 'HARVEST_PBT2021']]
+workflows[2.4] = ['PBT2021', ['ZeroBias2021', 'RECOPE_PBT21', 'HARVEST_PBT2021']]
+workflows[2.5] = ['PBT2021', ['HLTPhysics2021', 'HLT_PBT2021', 'RECO_PBT2021', 'HARVEST_PBT2021']]
+workflows[2.6] = ['PBT2021', ['HLTPhysics2021', 'RECOPE_PBT21', 'HARVEST_PBT2021']]
+
+# MWGR 2022
+workflows[3.1] = ['MWGR2022', ['HcalNZS2022', 'HLT_MWGR2022', 'RECO_MWGR2022', 'HARVEST_MWGR2022']]
+workflows[3.2] = ['MWGR2022', ['HcalNZS2022', 'RECO_MWGR2022', 'HARVEST_MWGR2022']]
+
+# CRAFT 2022
+workflows[4.1] = ['CRAFT2022', ['HLTPhysics2022', 'HLT_CRAFT22', 'RECO_CRAFT2022', 'HARVEST_CRAFT22']]
+workflows[4.2] = ['CRAFT2022', ['HLTPhysics2022', 'RECO_CRAFT2022', 'HARVEST_CRAFT22']]
+workflows[4.3] = ['', ['Cosmics2022', 'HLT_CRAFT22', 'RECO_CRAFT2022', 'HARVEST_CRAFT22']]
+workflows[4.4] = ['', ['Cosmics2022', 'RECO_CRAFT2022', 'HARVEST_CRAFT22']]
+
+
+# MRH Test for Express WF
+workflows[5.1] = ['MRHTest22', ['MinimumBias2021', 'RECO_MRH_Test', 'ALCARECO_MRH_Test']]
